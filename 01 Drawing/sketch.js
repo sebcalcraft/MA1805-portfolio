9fbda23649a9e2704e79cb5cd93004c19bbb5ddf
sebcalcraft <zpwf303@live.rhul.ac.uk>
2025-11-05 07:22:50 +0000

    commit

--- 01 Drawing/sketch.js
+++ 01 Drawing/sketch.js
@@ -7,12 +7,10 @@ function draw(){
   background(15,17,21);
   translate(width/2, height/2);
 
-
   noStroke();
   fill(0,0,0,190);
   ellipse(-120, -150, 360, 300);
 
-  
   push();
   translate(-60, -40);
   
@@ -26,36 +24,29 @@ function draw(){
   }
   endShape(CLOSE);
 
-
   triangle(65,25, 135,40, 75,60);
-
-
   fill(0);
   textAlign(CENTER, CENTER);
   textSize(32);
   text("@#!", 0, 0);
   pop();
 
- 
   push();
   translate(100, 100);
   fill(255,210,80);
   stroke(0,50);
   strokeWeight(2);
   circle(0,0,160);
-
   
   stroke(0,180);
   strokeWeight(10);
   point(-35,-20);
   point(35,-20);
 
-
   noFill();
   stroke(0,180);
   strokeWeight(12);
   arc(0, 25, 80, 50, 210, 330);
-
 
   noStroke();
   fill(90,160,255);
