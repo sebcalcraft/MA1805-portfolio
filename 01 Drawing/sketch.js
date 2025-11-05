46b4f288ac070db789e98abff495cd0d73dbda55
sebcalcraft <zpwf303@live.rhul.ac.uk>
2025-11-05 07:24:23 +0000

    commit

--- 01 Drawing/sketch.js
+++ 01 Drawing/sketch.js
@@ -1,55 +1,34 @@
-function setup(){
-  createCanvas(520, 520);
-  angleMode(DEGREES);
-  noLoop(); 
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
 }
-function draw(){
-  background(15,17,21);
-  translate(width/2, height/2);
 
-  noStroke();
-  fill(0,0,0,190);
-  ellipse(-120, -150, 360, 300);
+function draw() {
+  background(200);
 
-  push();
-  translate(-60, -40);
-  
-  fill(250,95,95);
-  beginShape();
-  let r = 90;
-  for (let a = 0; a < 360; a += 30){
-    let spike = (a % 60 === 0) ? 25 : -5;
-    let rr = r + spike;
-    vertex(rr * cos(a), rr * sin(a));
-  }
-  endShape(CLOSE);
+  // big bully hill thing
+  fill(50);
+  ellipse(120, 120, 250, 200);
 
-  triangle(65,25, 135,40, 75,60);
+  // mean speech bubble
+  fill(255);
+  ellipse(200, 100, 150, 100);
+  triangle(160, 120, 180, 130, 170, 140);
   fill(0);
-  textAlign(CENTER, CENTER);
   textSize(32);
-  text("@#!", 0, 0);
-  pop();
+  text("@!", 180, 110);
 
-  push();
-  translate(100, 100);
-  fill(255,210,80);
-  stroke(0,50);
-  strokeWeight(2);
-  circle(0,0,160);
-  
-  stroke(0,180);
-  strokeWeight(10);
-  point(-35,-20);
-  point(35,-20);
+  // small scared face
+  fill(255, 255, 0);
+  ellipse(300, 280, 100, 100);
 
+  // eyes
+  fill(0);
+  ellipse(285, 270, 10, 10);
+  ellipse(315, 270, 10, 10);
+
+  // sad mouth
   noFill();
-  stroke(0,180);
-  strokeWeight(12);
-  arc(0, 25, 80, 50, 210, 330);
-
-  noStroke();
-  fill(90,160,255);
-  circle(33, -2, 16);
-  pop();
+  stroke(0);
+  arc(300, 300, 40, 20, PI, 0);
 }
