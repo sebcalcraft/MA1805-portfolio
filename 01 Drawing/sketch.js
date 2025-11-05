fe9af53a171b40806ce74b7a9df1c1d8158d26d0
sebcalcraft <zpwf303@live.rhul.ac.uk>
2025-11-05 07:02:03 +0000

    commit

---
+++ 01 Drawing/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(640, 480); 
+  background(230);    
+  // noStroke();              
+}
+
+function draw(){
+    fill(0); 
+    rect(0, 0, 100, 100); // x, y, w, h
+    
+    fill(64);
+		rect(50, 50, 100, 100); 
+
+		fill(128)
+		rect(100, 100, 100, 100);
+		
+		fill(196);
+		rect(150, 150, 200, 200);
+
+		fill(220);
+		rect(300, 300, 400, 400);
